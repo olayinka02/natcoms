0f995dc3008f984ca7ee8b9f877fd0ade15b1259
Olayinka <64751867+olayinka02@users.noreply.github.com>
2022-12-08 18:00:58 +0100

    natcom

---
+++ index.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    $('#nav-icon3').click(function () {
+      $(this).toggleClass('open');
+    });
+  });
+
+
+
+  $(document).ready(function () {
+    $('#nav-icon3x').click(function () {
+      $(this).toggleClass('open');
+    });
+  });
